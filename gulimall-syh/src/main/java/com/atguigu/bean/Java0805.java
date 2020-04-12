34c64f2ebf8a7ee96367744780a892f588606b34
syh <767884144@qq.com>
2020-04-12 10:31:36 +0800

    王芬是个笨蛋

---
+++ gulimall-syh/src/main/java/com/atguigu/bean/Java0805.java
@@ -0,0 +1,13 @@
+package com.atguigu.bean;
+
+import java.sql.SQLOutput;
+
+/**
+ * @author shkstart
+ * @create 2020-04-12 10:30
+ */
+public class Java0805 {
+    public static void main(String[] args) {
+        System.out.println("王芬是个笨蛋");
+    }
+}
