e10c58df45563ee1146329747396ca31e95821b8
syh <767884144@qq.com>
2020-04-12 12:45:39 +0800

    李四来了

--- gulimall-syh/src/main/java/com/atguigu/bean/Java0806.java
+++ gulimall-syh/src/main/java/com/atguigu/bean/Java0806.java
@@ -8,5 +8,7 @@ public class Java0806 {
     public static void main(String[] args) {
         System.out.println("张三入职第一天");
         System.out.println("coding。。。。");
+        //------以下是李四
+        System.out.println("李四来了------");
     }
 }
