39e21c285f1cd64740fccfa9f96d72e611160417
syh <767884144@qq.com>
2020-04-12 10:45:17 +0800

    大家好，我是0806的人

---
+++ gulimall-syh/src/main/java/com/atguigu/bean/Java0806.java
@@ -0,0 +1,11 @@
+package com.atguigu.bean;
+
+/**
+ * @author shkstart
+ * @create 2020-04-12 10:41
+ */
+public class Java0806 {
+    public static void main(String[] args) {
+        System.out.println("张三入职第一天");
+    }
+}
